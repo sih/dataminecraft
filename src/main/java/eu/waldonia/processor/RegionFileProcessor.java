6942435412dbed445a67e3c896cd3e9f06a33fac
Sid Haniff <sid.haniff@yahoo.com>
2015-07-05 18:01:52 +0100

    Map the header part of the MC region file. This contains the addresses of the chunks within the file

--- src/main/java/eu/waldonia/processor/RegionFileProcessor.java
+++ src/main/java/eu/waldonia/processor/RegionFileProcessor.java
@@ -3,8 +3,8 @@ package eu.waldonia.processor;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,26 +15,31 @@ import java.util.Map;
  */
 public class RegionFileProcessor {
 	
-	public static final String MCA = "mca";
+	public static final String MCA = ".mca";
 	public static final int CHUNK_BYTEMAP_SIZE = 4096;
+	private static final int CHUNK_BYTEMAP_ITEM_SIZE = 4;
+	private static final int CHUNK_SECTOR_SIZE = 4096;
 
 	/**
 	 * Make a map of the chunks held in the input file
 	 * @param f The input region file. Will validate this is an .mca file
 	 * @return A map of chunk locations by their byte offset or an empty map if the file is not valid
 	 */
-	public Map<Integer,String> mapChunks(final File f) {
-		Map<Integer,String> map = new HashMap<>();
+	public Map<Integer,Integer> mapChunks(final File f) {
+		Map<Integer,Integer> map = new HashMap<>();
 		// basic validity checks
 		if (f != null && !f.isDirectory()) {
 			// slightly more sophisticated validity checks
-			String[] fileParts = f.getName().split("\\.");
-			if (fileParts != null && fileParts.length == 2) {
-				if (MCA.equals(fileParts[1])) {
-					// TODO remove this stub
-					map.put(1, "Yay");
+			if (f.getName().contains(".")) {
+				String fileExtension = f.getName().substring(f.getName().lastIndexOf("."));
+				if (fileExtension != null && MCA.equals(fileExtension)) {
+					try {
+						map.putAll(processMap(f));
+					}
+					catch(Exception e) {
+						e.printStackTrace();
+					}
 				}
-				
 			}
 			
 		}
@@ -44,21 +49,32 @@ public class RegionFileProcessor {
 	}
 	
 	
-	private Map<Integer,String> processMap(final File f) throws Exception {
-		Map<Integer,String> map = new HashMap<>();
+	/**
+	 * @see http://minecraft.gamepedia.com/Region_file_format
+	 * @param f The region file to process. Guaranteed not null
+	 * @return A map of MC sector sizes keyed by the byte location of their chunk
+	 * @throws Exception when the file cannot be found or there is an error processing it
+	 */
+	private Map<Integer,Integer> processMap(final File f) throws Exception {
+		Map<Integer,Integer> map = new HashMap<>();
 		
 		try(FileInputStream fis = new FileInputStream(f);
 			BufferedInputStream bis = new BufferedInputStream(fis)) {
 		
 			byte[] chunkByteMap = new byte[CHUNK_BYTEMAP_SIZE];
 			bis.read(chunkByteMap);
+			// iterate through each logical item
+			for (int offset = 0; offset < CHUNK_BYTEMAP_SIZE / CHUNK_BYTEMAP_ITEM_SIZE; offset+=CHUNK_BYTEMAP_ITEM_SIZE) {
+				byte[] item = Arrays.copyOfRange(chunkByteMap, offset, offset+CHUNK_BYTEMAP_ITEM_SIZE);
+			
+				// first 3 bytes represent the address - shift and mask to turn them in to ints (they're big-endian)
+				int mcChunkByteAddress = ((item[0] & 0xFF) << 16 | (item[1] & 0xFF) << 8 | (item[2] & 0xFF)) * CHUNK_SECTOR_SIZE;
+				int mcSectorSize = item[3];
+				
+				map.put(mcChunkByteAddress,mcSectorSize);
+			}
 			
 		}
-		
-		
-		
-		
-		
 		return map;
 	}
 
--- src/test/java/eu/waldonia/processor/RegionFileProcessorTest.java
+++ src/test/java/eu/waldonia/processor/RegionFileProcessorTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.io.File;
 import java.util.Map;
+import java.util.Set;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -12,6 +13,7 @@ public class RegionFileProcessorTest {
 
 	private RegionFileProcessor rfp;
 	private File f;
+	private static final String VALID_REGION_FILE_NAME = "./src/test/resources/r.0.-1.mca";
 	
 	@Before
 	public void setUp() {
@@ -21,29 +23,43 @@ public class RegionFileProcessorTest {
 	@Test
 	public void mapChunksShouldReturnAnEmptyMapIfNullFileSupplied() {
 		f = null;
-		Map<Integer,String> chunkMap = rfp.mapChunks(f);
+		Map<Integer,Integer> chunkMap = rfp.mapChunks(f);
 		assertTrue(chunkMap.isEmpty());
 	}
 
 	@Test
 	public void mapChunksShouldReturnAnEmptyMapIfNoFileExtension() {
 		f = new File("../test/resources/boohoo");
-		Map<Integer,String> chunkMap = rfp.mapChunks(f);
+		Map<Integer,Integer> chunkMap = rfp.mapChunks(f);
 		assertTrue(chunkMap.isEmpty());
 	}
 	
 	@Test
 	public void mapChunksShouldReturnAnEmptyMapIfNonMCAFileExtension() {
-		f = new File("../test/resources/boohoomca");
-		Map<Integer,String> chunkMap = rfp.mapChunks(f);
+		f = new File("./test/resources/boohoomca");
+		Map<Integer,Integer> chunkMap = rfp.mapChunks(f);
 		assertTrue(chunkMap.isEmpty());
 	}
 	
 	@Test
-	public void mapChunksShouldReturnDataForValidFile() {
-		f = new File("../test/resources/boohoo.mca");
-		Map<Integer,String> chunkMap = rfp.mapChunks(f);
+	public void mapChunksShouldReturnDataForValidFile() throws Exception {
+		f = new File(VALID_REGION_FILE_NAME);
+		Map<Integer,Integer> chunkMap = rfp.mapChunks(f);
 		assertFalse(chunkMap.isEmpty());
+	}
+	
+	@Test
+	public void mapChunksShouldReturnNonNegativeAddresses() throws Exception {
+		f = new File(VALID_REGION_FILE_NAME);
+		Map<Integer,Integer> chunkMap = rfp.mapChunks(f);
+		assertFalse(chunkMap.isEmpty());
+		Set<Integer> addresses = chunkMap.keySet();
+		for (Integer addr : addresses) {
+			if (addr <= 0) {
+				fail("Address was "+addr+" but should be non-negative");
+			}
+		}
+		
 	}
 	
 }
